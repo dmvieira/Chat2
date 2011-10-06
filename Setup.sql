65af7cfc178b1d326a6913df1d8f215bfdb2e56d
dmvieira <diogo.mvieira@gmail.com>
2011-10-06 17:33:20 -0300

    Change some varchars to chars to improve BD I/Os and change chars to utf8

--- Setup.sql
+++ Setup.sql
@@ -1,24 +1,38 @@
 CREATE TABLE IF NOT EXISTS `chat_rooms` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
+  `name` char(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `numofuser` int(10) NOT NULL,
-  `file` varchar(30) NOT NULL,
+  `file` char(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `chat_users`
+--
 
 CREATE TABLE IF NOT EXISTS `chat_users` (
   `id` tinyint(10) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
+  `username` char(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `status` tinyint(1) NOT NULL,
   `time_mod` int(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `chat_users_rooms`
+--
 
 CREATE TABLE IF NOT EXISTS `chat_users_rooms` (
   `id` int(100) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  `room` varchar(100) NOT NULL,
+  `username` char(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `room` char(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `mod_time` int(40) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1472 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1502 ;
